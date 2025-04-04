467b946824f85cd89a1be9b0b3b353dd70a89af8
Vítor Silva Borsato <viborsato21@gmail.com>
2025-04-03 22:32:39 -0300

    asdsad

---
+++ aula01/app.js
@@ -0,0 +1,18 @@
+// alert("Hello World!")
+
+console.log("mensagem");
+
+let nome = prompt("Qual é o seu nome?")
+// let result = confirm("Você deseja seguir? ")
+console.log(`Olá ${nome}, tudo certo?` )
+let teste = "sdfdsf" / 6
+const apikey = 1/0
+let test = null
+let testa 
+
+
+console.log(typeof nome)
+console.log(apikey)
+console.log(teste)
+console.log(test)
+console.log(testa)
---
+++ aula01/ex1.js
@@ -0,0 +1,5 @@
+let nome = prompt("Qual o seu nome? ")
+let idade = prompt("Qual a sua idade? ")
+let profissao = prompt("Qual a sua profisão? ")
+
+alert(`Ola ${nome}, voce tem ${idade} e sua profissão é ${profissao}`)
